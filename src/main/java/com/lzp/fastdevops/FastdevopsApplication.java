19b8f9fbf63d6de183cae7b06807141f0c5ccf22
Aygx <277980904@qq.com>
2018-08-20 21:04:15 +0800

    启动fastDevops项目
    Signed-off-by: Aygx <277980904@qq.com>

---
+++ src/main/java/com/lzp/fastdevops/FastdevopsApplication.java
@@ -0,0 +1,12 @@
+package com.lzp.fastdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastdevopsApplication.class, args);
+    }
+}
